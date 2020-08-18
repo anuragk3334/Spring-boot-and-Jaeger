02066b56c8eb7112a1818c7958988be2899eb2b9
Anurag Kumar <akumar293@slb.com>
2020-08-18 20:05:42 +0530

    commit

--- HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import io.jaegertracing.Configuration;
-import io.jaegertracing.Configuration.ReporterConfiguration;
-import io.jaegertracing.Configuration.SamplerConfiguration;
 import io.jaegertracing.internal.JaegerTracer;
 import io.jaegertracing.internal.MDCScopeManager;
 import lombok.extern.slf4j.Slf4j;
@@ -20,29 +17,30 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
-
-	// @Override
+	
+	//@Override
 	public void run(ApplicationArguments applicationArguments) throws Exception {
 		log.debug("Debugging log");
 		log.info("Info log");
 		log.warn("Hey, This is a warning!");
 		log.error("Oops! We have an Error. OK");
-
+		
 	}
-
+	
 	@Bean
 	public io.opentracing.Tracer init() {
-
-		SamplerConfiguration samplerConfig = SamplerConfiguration.fromEnv().withType("const").withParam(1);
-		ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true);
-		MDCScopeManager scopeManager = new MDCScopeManager.Builder().build();
-		Configuration config = Configuration.fromEnv("boot-service-a").withSampler(samplerConfig)
-				.withReporter(reporterConfig);
-		return config.getTracer();
-
 		
-		//return new JaegerTracer.Builder("boot-service-c").withScopeManager(scopeManager).build();
-
+		/*
+		 * SamplerConfiguration samplerConfig =
+		 * SamplerConfiguration.fromEnv().withType("const").withParam(1);
+		 * ReporterConfiguration reporterConfig =
+		 * ReporterConfiguration.fromEnv().withLogSpans(true); Configuration config =
+		 * Configuration.fromEnv("boot-service-a").withSampler(samplerConfig).
+		 * withReporter(reporterConfig); return config.getTracer();
+		 */
+		MDCScopeManager scopeManager = new MDCScopeManager.Builder().build();
+		return new JaegerTracer.Builder("boot-service-c").withScopeManager(scopeManager).build();
+		
 	}
 
 }
